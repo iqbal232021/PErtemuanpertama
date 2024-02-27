eaf875c39bf28f1f689819c93efac99a75a0f1cb
iqbal232021 <furqoniqbal472@gmail.com>
2024-02-27 18:49:47 +0700

    Add project files.

---
+++ PErtemuanpertama/PErtemuanpertama.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin 
+	//numeric nJejari, nLuas
+	float nJejari, nLuas;
+	//display 'Masukan Jejari = '
+	cout << "Masukkan jejari = ";
+	//accept nJejari
+	cin >> nJejari;
+	//compute nLuas = 3,14 * r * r
+	nLuas = 3.14 * nJejari * nJejari;
+	//display 'luasnya= ' + nLuas
+	cout << "Luasnya =" << nLuas << endl;
+
+
+	
+
+
+   
+}//end
+
+
